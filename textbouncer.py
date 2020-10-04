26f19be4fe05eabac237cb6a0408c45132056aa4
jackweatherford <jack.weatherford@gmail.com>
2020-10-04 00:21:21 -0700

    Fixed a performance bug

--- textbouncer.py
+++ textbouncer.py
@@ -10,16 +10,19 @@ import win32gui
 # Other
 from random import randint
 
-WIN_WIDTH = 500 # Window Width
-WIN_HEIGHT = 500 # Window Height
+WIN_WIDTH = 1280 # Window Width
+WIN_HEIGHT = 720 # Window Height
 
-INIT_VEL = 0.5 # Initial Velocity - How fast the text moves
+INIT_VEL = 0.1 # Initial Velocity - How fast the text moves
 
-FONT_SIZE = 50 # How big the text is
+# Name of a popular font OR relative filepath to a .ttf file
+FONT_NAME = 'impact'
+
+FONT_SIZE = 30 # How big the text is
 FONT_Y_MARGIN = FONT_SIZE // 10 # Text y hitbox margin
 FONT_COLOR = (194, 24, 7) # Color of the text
 
-FUCHSIA = (255, 0, 128)  # Transparency color
+BLACK = (1, 1, 1)  # Transparency color
 
 class Text:
 	def __init__(self, img, w, h, x, y, x_vel, y_vel):
@@ -33,7 +36,7 @@ class Text:
 
 # Gets the text to bounce around from user
 def getText():
-	font = pygame.font.SysFont(None, FONT_SIZE) # Initialize font object
+	font = pygame.font.SysFont(FONT_NAME, FONT_SIZE) # Initialize font object
 	
 	# Dummy blank Text that stays in the middle of the screen
 	dummy = Text(font.render('', True, FONT_COLOR), 0, 0, WIN_WIDTH/2, WIN_HEIGHT/2, 0, 0)
@@ -51,10 +54,26 @@ def getText():
 				h = img.get_height()
 				
 				# Randomize starting position
-				x = randint(0, WIN_WIDTH - w)
-				y = randint(0, WIN_HEIGHT - h)
+				x = randint(10, WIN_WIDTH - w - 10)
+				y = randint(10, WIN_HEIGHT - h - 10)
+				
+				# Randomize starting direction
+				dir = randint(1, 4)
+				if dir == 1:
+					x_vel = INIT_VEL
+					y_vel = INIT_VEL
+				elif dir == 2:
+					x_vel = -INIT_VEL
+					y_vel = INIT_VEL
+				elif dir == 3:
+					x_vel = -INIT_VEL
+					y_vel = -INIT_VEL
+				else:
+					x_vel = INIT_VEL
+					y_vel = -INIT_VEL
+				
 				# Add text to list
-				texts.append(Text(img, w, h, x, y, INIT_VEL, INIT_VEL))
+				texts.append(Text(img, w, h, x, y, x_vel, y_vel))
 	
 	return texts
 
@@ -74,22 +93,24 @@ def main():
 	# Set window transparency color
 	hwnd = pygame.display.get_wm_info()['window']
 	win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
-	win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*FUCHSIA), 0, win32con.LWA_COLORKEY)
+	win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*BLACK), 0, win32con.LWA_COLORKEY)
 	
 	done = False
 	while not done:
 		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				done = True
+				exit()
 			if event.type == pygame.VIDEORESIZE:
 				WIN_WIDTH = event.w
 				WIN_HEIGHT = event.h
 				screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT), pygame.RESIZABLE)
 				texts[0].w = WIN_WIDTH/2
 				texts[0].h = WIN_HEIGHT/2
-			if event.type == pygame.QUIT:
-				done = True
-				exit()
 		
-		screen.fill(FUCHSIA) # Set transparent background
+		screen.fill(BLACK) # Set transparent background
+		
+		dt = clock.tick(60)
 		
 		for text in texts: # Draw Texts + check window edge collision
 			if text.x + text.w >= WIN_WIDTH:
@@ -105,8 +126,6 @@ def main():
 			elif text.y + FONT_Y_MARGIN <= 0:
 				text.y_vel = INIT_VEL
 				text.y = -FONT_Y_MARGIN
-			
-			dt = clock.tick(1000)
 			
 			text.x += text.x_vel * dt
 			text.y += text.y_vel * dt
